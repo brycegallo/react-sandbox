8aa6929d126dd2df2ba8833cd053cca74022530f
bryce <76966016+brycegallo@users.noreply.github.com>
2023-05-11 13:21:50 -0400

    Finish Example1

---
+++ src/components/UseRefExample1.jsx
@@ -0,0 +1,31 @@
+import { useRef } from 'react'
+
+function UseRefExample1() {
+  const inputRef = useRef()
+  const paraRef = useRef()
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(inputRef.current.value)
+    inputRef.current.value = `Hello ${inputRef.current.value}`
+    paraRef.current.innerText = 'Something Submitted'
+  }
+
+  return (
+    <div>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            ref={inputRef}
+            id="name"
+            className="form-control mb-2"
+          />
+          <button type="submit" className='btn btn-primary'>Submit</button>
+          <p onClick={() => inputRef.current.focus()} ref={paraRef}>Nothing Submitted</p>
+        </form>
+    </div>
+  )
+}
+
+export default UseRefExample1
